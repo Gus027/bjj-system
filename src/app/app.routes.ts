8fc4f7834eab93837dfe8fbdb09215de8e13c495
Gustavo Gomes Ferreira <102599068+Gus027@users.noreply.github.com>
2025-09-21 20:21:57 -0300

    feat: Abas de navegação + Scarlone

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
@@ -14,6 +12,33 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./dashboard/dashboard/dashboard.component').then(
         (m) => m.DashboardComponent
+      ),
+  },
+  {
+    path: 'alunos',
+    loadComponent: () =>
+      import('./pages/alunos/alunos.component').then((m) => m.AlunosComponent),
+  },
+  {
+    path: 'presenca',
+    loadComponent: () =>
+      import('./pages/presenca/presenca.component').then((m) => m.PresencaComponent),
+  },
+  {
+    path: 'eventos',
+    loadComponent: () =>
+      import('./pages/eventos/eventos.component').then((m) => m.EventosComponent),
+  },
+  {
+    path: 'posts',
+    loadComponent: () =>
+      import('./pages/posts/posts.component').then((m) => m.PostsComponent),
+  },
+  {
+    path: 'notificacoes',
+    loadComponent: () =>
+      import('./pages/notificacoes/notificacoes.component').then(
+        (m) => m.NotificacoesComponent
       ),
   },
   {
